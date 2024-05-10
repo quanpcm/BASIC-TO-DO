f0f0185e482a7868a6664b654d4906ad7726c17a
kenroly <ngthedat2002@gmail.com>
2024-05-10 19:38:02 +0700

    Add something

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QWidget, QListWidgetItem, QPushButton
 from PyQt6 import uic
 import sys
 
@@ -8,7 +8,18 @@ class Main(QtWidgets.QMainWindow):
         super().__init__()
         uic.loadUi('gui/main.ui', self)
         
-        # self.bt_todo1.clicked.connect(self.show_todo1)
+        # for key, value in todo_dict.items():
+        #     self.todoList.addItem(f'{key} - {value}')
+        self.populateList()
+
+    def populateList(self):
+        for key, value in todo_dict.items():
+            form_widget = uic.loadUi('gui/form.ui')
+            form_widget.btn_todo.setText(f'{key} - {value}')
+            listWidgetItem = QListWidgetItem(self.todoList)
+            listWidgetItem.setSizeHint(form_widget.sizeHint())
+            self.todoList.addItem(listWidgetItem)
+            self.todoList.setItemWidget(listWidgetItem, form_widget)
         
     def show_detail(self):
         detail.show()
@@ -28,6 +39,16 @@ class Detail(QtWidgets.QMainWindow):
         self.close()
       
 if __name__ == '__main__':
+    
+    import sqlite3
+    conn = sqlite3.connect('todo.db')
+    cursor = conn.cursor()
+    query = "SELECT * FROM todo;"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    todo_dict = {row[0]: row[1] for row in rows}
+    print(todo_dict)
+    conn.close()
     
     app = QtWidgets.QApplication(sys.argv)
     
