3654cf646529f4748a04f0ef1fbe0abccccac3fb
quanpcm <135415339+quanpcm@users.noreply.github.com>
2024-05-15 18:24:36 +0700

    sua code

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QWidget, QListWidgetItem, QPushButton
+from PyQt6.QtWidgets import QWidget, QListWidgetItem, QPushButton,QMessageBox
 from PyQt6 import uic
 import sys
 
@@ -7,14 +7,12 @@ class Main(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('gui/main.ui', self)
-        todo = uic.loadUi('gui/form.ui')
         # for key, value in todo_dict.items():
         #     self.todoList.addItem(f'{key} - {value}')
         
         self.populateList()
 
         self.btn_plus.clicked.connect(self.populateList)
-        self.todo.clicked.connect(self.show_detail)
 
     def populateList(self):
         count = 1
@@ -43,7 +41,39 @@ class Detail(QtWidgets.QMainWindow):
     def show_Main(self):
         MainPage.show()
         self.close()
-      
+
+class Login(QtWidgets.QMainWindow): 
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('gui/login.ui', self)
+
+        self.btn_login.clicked.connect(self.login)
+        self.btn_register.clicked.connect(self.showRegister)
+        
+    def showRegister(self):
+        RegisterPage.show()
+        self.close()
+        
+    def login(self):
+        email = self.txt_name.QPlainTextEdit()
+        password = self.txt_pass.QPlainTextEdit()
+        if not email:
+            msg_box.setText('please enter username')
+            msg_box.exec()
+            return
+        if not password:
+            msg_box.setText('please enter password')
+            msg_box.exec()
+            return
+        if email == 'minhquan' and password == '09090909':
+            self.close()
+            MainPage.show()
+        
+class Register(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('gui/register.ui', self)
+        
 if __name__ == '__main__':
     import sqlite3
     conn = sqlite3.connect('todo.db')
@@ -59,7 +89,14 @@ if __name__ == '__main__':
     
     detail = Detail()
     MainPage = Main()
+    LoginPage = Login()
+    RegisterPage = Register()
 
-    MainPage.show()
+    LoginPage.show()
+    
+    msg_box = QMessageBox()
+    msg_box.setWindowTitle('!!!SOMETHING WRONG!!!')
+    msg_box.setIcon(QMessageBox.Icon.Critical)
+    msg_box.setStyleSheet("background-color: white; color: black;")
     
     app.exec()    
